437fd8afab2d6aa9e3c077818d8478e2c4a3d212
Nicolas Gomez <77043795+niicccoooo@users.noreply.github.com>
2022-01-09 03:21:40 -0300

    Initializing project using create-react-app

---
+++ hot_dog/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TESTING</h1>
+
+      <Header />
+      {/* Matches */}
+      {/* InteractButtons */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ hot_dog/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+            <h2>I am the header.</h2>
+        </div>
+    )
+}
+
+export default Header
