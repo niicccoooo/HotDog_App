1d4f14ec1246a9a8e0e6f4be4f8ac35b5b11c512
Nicolas Gomez <77043795+niicccoooo@users.noreply.github.com>
2022-01-15 00:54:46 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ChatIcon from '@mui/icons-material/Chat';
+import IconButton from '@mui/material/IconButton';
+
+
+function Header() {
+    return (
+        <div className="header">
+    
+            <IconButton>
+                <AccountBoxIcon fontSize="large" />
+            </IconButton>
+
+        <img className="logo"
+             src="https://dl5zpyw5k3jeb.cloudfront.net/organization-photos/42559/2/?bust=1511995536"
+             alt="Dog logo"
+            />
+
+            <IconButton>
+                <ChatIcon fontSize="large" />
+            </IconButton>
+
+        </div>
+    )
+}
+
+export default Header
